77bc74fcde33efdfda902c240e53228c5aeab50b
Ludovit <ludovit@Ludovits-MacBook-Pro-M3.local>
2024-04-16 10:39:03 +0200

    Added task service

---
+++ src/main/java/sk/krytoss/rest/todo/service/TaskService.java
@@ -0,0 +1,28 @@
+package sk.krytoss.rest.todo.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import sk.krytoss.rest.todo.model.Task;
+import sk.krytoss.rest.todo.repository.TaskRepository;
+
+import java.util.List;
+
+@Service
+public class TaskService {
+
+    @Autowired
+    TaskRepository taskRepository;
+
+    public Task createTask(Task task) {
+        return taskRepository.save(task);
+    }
+
+    public List<Task> getTasks() {
+        return taskRepository.findAll();
+    }
+
+    public void deleteTask(Long taskId) {
+        taskRepository.deleteById(taskId);
+    }
+
+}
